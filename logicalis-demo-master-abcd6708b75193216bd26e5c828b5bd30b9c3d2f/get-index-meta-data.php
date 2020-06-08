e4a4094916352a3a045e02e2657e9520db3b8600
joseluis <bgjoseluis@gmail.com>
2020-06-08 22:04:17 +0200

    200608

---
+++ logicalis-demo-master-abcd6708b75193216bd26e5c828b5bd30b9c3d2f/get-index-meta-data.php
@@ -0,0 +1,17 @@
+<?php
+
+  echo "<table class='table table-bordered'>";
+  echo "<tr><th>Meta-Data</th><th>Value</th></tr>";
+
+  #The URL root is the AWS meta data service URL where metadata
+  # requests regarding the running instance can be made
+  $urlRoot="http://169.254.169.254/latest/meta-data/";
+  # Get the instance ID from meta-data and print to the screen
+  echo "<tr><td>Hostname</td><td><i>" .  gethostname() . "</i></td><tr>";
+
+  # Availability Zonae
+  echo "<tr><td> Local IP</td><td><i>" . getHostByName(getHostName())  . "</i></td><tr>";
+
+  echo "</table>";
+
+?>
